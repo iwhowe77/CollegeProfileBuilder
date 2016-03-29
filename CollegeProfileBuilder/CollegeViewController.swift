8be519b714b6878f9a4adb7ad9b927c3ccb70f0c
aporter6331 <aporter6331@bg-mmcte-21.d214.org>
2016-03-29 08:50:29 -0500

    segue code

--- CollegeProfileBuilder/CollegeViewController.swift
+++ CollegeProfileBuilder/CollegeViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class CollegeViewController: UIViewController {
+class CollegeViewController: UITableViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -18,6 +21,30 @@ class CollegeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath)
+        {
+            
+            
+        }
+        print(1)
+        performSegueWithIdentifier("moreinfo", sender: nil)
+        
+                
+        
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "moreinfo"
+        {
+           print(2)
+            
+        }
     }
 
 
---
+++ CollegeProfileBuilder/collegeInfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  collegeInfoViewController.swift
+//  CollegeProfileBuilder
+//
+//  Created by Andrew A. Porter on 3/29/16.
+//  Copyright © 2016 Ian W. Howe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class collegeInfoViewController: UIViewController
+{
+    
+}
