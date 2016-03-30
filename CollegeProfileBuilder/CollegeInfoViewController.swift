568fa052b846f77ff8822364906879ed98c171c8
ihowe6189 <ihowe6189@stu.d214.org>
2016-03-30 08:27:50 -0500

    Manual Sync 1

---
+++ CollegeProfileBuilder/CollegeInfoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollegeInfoViewController.swift
+//  CollegeProfileBuilder
+//
+//  Created by Ian W. Howe on 3/30/16.
+//  Copyright © 2016 Ian W. Howe. All rights reserved.
+//
+
+import Foundation
