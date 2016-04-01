dabb5d62a83bec50e7d91bb6dfb9c3d4d94b0876
ihowe6189 <ihowe6189@stu.d214.org>
2016-04-01 09:06:54 -0500

    Stretch 4 Finished

--- CollegeProfileBuilder/CollegeViewController.swift
+++ CollegeProfileBuilder/CollegeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CollegeViewController: UITableViewController {
     
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
     var collegeList: [College]
     var selectedCollegeIndex = -1
 
@@ -42,6 +44,7 @@ class CollegeViewController: UITableViewController {
         return collegeList.count
     }
     
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         //var cell = tableView.dequeueReusableCellWithIdentifier("CollegeItem")
@@ -55,6 +58,7 @@ class CollegeViewController: UITableViewController {
 
         }
         
+        //Stretch 1
         cell!.detailTextLabel!.text = "some text"
         let college = collegeList[indexPath.row]
         cell!.detailTextLabel!.text = college.location
@@ -89,7 +93,7 @@ class CollegeViewController: UITableViewController {
         }
     }
     
-    
+    //Stretch 2
     @IBAction func addCollege(sender: UIBarButtonItem) {
         
         let alert = UIAlertController(title: "College Info", message: "Press Done to add college to table ", preferredStyle: .Alert)
@@ -99,9 +103,9 @@ class CollegeViewController: UITableViewController {
                 sender in
                 let textField = alert.textFields![0] as UITextField
                 print("Text field: \(textField.text)")
-                let newItem = College(newName: alert.textFields![0].text!, newLocation: alert.textFields![1].text!, newNumberOfStudents: Int(alert.textFields![2].text!)!, newImage: UIImage())
-                self.collegeList.append(newItem)
+                let newItem = College(newName: alert.textFields![0].text!, newLocation: alert.textFields![1].text!, newNumberOfStudents: Int(alert.textFields![2].text!)!, newImage: UIImage(named: "placeHolder.png")!)
                 let newRowIndex = self.collegeList.count
+                self.collegeList.append(newItem)
                 let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
                 let indexPaths = [indexPath]
                 self.tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
@@ -121,8 +125,33 @@ class CollegeViewController: UITableViewController {
         
     }
     
+    //Stretch 3
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        collegeList.removeAtIndex(indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
+    }
+
+    //Stretch 4
+    @IBAction func startEditing(sender: UIBarButtonItem) {
+        self.editing = !self.editing
+        if self.editing {
+            editBarButton.title = "Done"
+        }
+        else {
+            editBarButton.title = "Edit"
+        }
+    }
     
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
     
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        let itemToMove = collegeList[fromIndexPath.row]
+        collegeList.removeAtIndex(fromIndexPath.row)
+        collegeList.insert(itemToMove, atIndex: toIndexPath.row)
+    }
     
     
     
